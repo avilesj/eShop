75f5fb6abd0325dda1602cbf505d701d8ff6aa58
jhokva <jlapmail@gmail.com>
2018-09-30 09:39:26 -0400

    Fixed CartItemService tests. Now all tests pass.

--- src/test/java/com/hercule/eshop/services/CartItemServiceTests.java
+++ src/test/java/com/hercule/eshop/services/CartItemServiceTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 @ActiveProfiles("dev")
 public class CartItemServiceTests
 {
