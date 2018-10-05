87c0f3d08b4c10c2ec8d97f41f713dcf2ea94160
jhokva <jlapmail@gmail.com>
2018-10-05 00:19:10 -0400

    Added user dashboard for admins

--- src/main/java/com/hercule/eshop/controllers/AdminController.java
+++ src/main/java/com/hercule/eshop/controllers/AdminController.java
@@ -13,4 +13,10 @@ public class AdminController
     {
         return "admin/adminIndex";
     }
+
+    @RequestMapping("/users")
+    public String getUserDashboard()
+    {
+        return "admin/adminUserDashboard";
+    }
 }
