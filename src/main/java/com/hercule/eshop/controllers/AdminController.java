9fb90ad9056009a32203ffd6a09f5843e43d97d0
Jose Aviles <jlapmail@gmail.com>
2018-10-05 08:07:26 -0400

    Added user search in admin user dashboard

--- src/main/java/com/hercule/eshop/controllers/AdminController.java
+++ src/main/java/com/hercule/eshop/controllers/AdminController.java
@@ -1,22 +1,40 @@
 package com.hercule.eshop.controllers;
 
 
+import com.hercule.eshop.models.User;
+import com.hercule.eshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController
 {
+    @Autowired
+    UserService userService;
+
     @RequestMapping("")
     public String getAdminIndex()
     {
         return "admin/adminIndex";
     }
 
-    @RequestMapping("/users")
+    @RequestMapping("/user")
     public String getUserDashboard()
     {
+        return "admin/adminUserDashboard";
+    }
+
+    @RequestMapping("/user/search")
+    public String searchUser(@RequestParam("userSearch") String username, Model model)
+    {
+        List<User> foundUsers = userService.searchUserByUsername(username);
+        model.addAttribute("foundUsers", foundUsers);
         return "admin/adminUserDashboard";
     }
 
