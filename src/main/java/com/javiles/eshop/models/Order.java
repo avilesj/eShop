89e2c11e1add49def4e6a9a2533b688c477142b6
Jose Aviles <jlapmail@gmail.com>
2018-10-29 05:33:49 -0400

    Order & OrderItem entity added.

---
+++ src/main/java/com/javiles/eshop/models/Order.java
@@ -0,0 +1,102 @@
+package com.javiles.eshop.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Order")
+public class Order
+{
+    @Id
+    @GeneratedValue
+    private long id;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "users_id")
+    private User user;
+    private LocalDateTime createdOn;
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER, cascade = CascadeType.DETACH, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private long size;
+    private String status;
+    private double total;
+
+    @PrePersist
+    public void setDateTimeOnCreate()
+    {
+        createdOn = LocalDateTime.now();
+    }
+
+    @PostLoad
+    public void calculateTotal()
+    {
+        this.total = orderItems.stream().mapToDouble(OrderItem::getTotal).sum();
+    }
+
+    public LocalDateTime getCreatedOn()
+    {
+        return createdOn;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public List<OrderItem> getOrderItems()
+    {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems)
+    {
+        this.orderItems = orderItems;
+    }
+
+    public long getSize()
+    {
+        return size;
+    }
+
+    public void setSize(long size)
+    {
+        this.size = size;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public double getTotal()
+    {
+        return total;
+    }
+
+    public void setTotal(double total)
+    {
+        this.total = total;
+    }
+}
---
+++ src/main/java/com/javiles/eshop/models/OrderItem.java
@@ -0,0 +1,98 @@
+package com.javiles.eshop.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "OrderItem")
+public class OrderItem
+{
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private LocalDateTime createdOn;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private long quantity;
+    private double price;
+    private double total;
+
+    @PrePersist
+    public void setDateTimeOnCreate()
+    {
+        createdOn = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedOn()
+    {
+        return createdOn;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public Order getOrder()
+    {
+        return order;
+    }
+
+    public void setOrder(Order order)
+    {
+        this.order = order;
+    }
+
+    public Product getProduct()
+    {
+        return product;
+    }
+
+    public void setProduct(Product product)
+    {
+        this.product = product;
+    }
+
+    public long getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public double getTotal()
+    {
+        return total;
+    }
+
+    public void setTotal(double total)
+    {
+        this.total = total;
+    }
+}
